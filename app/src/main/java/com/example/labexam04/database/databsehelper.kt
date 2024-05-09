d408a517b3c66d4251bf2fbeac968a0e7c67397b
Piruthivi <piruthivikulasegaram@gmail.com>
2024-05-09 11:47:59 +0530

    todolist

--- app/src/main/java/com/example/labexam04/database/databsehelper.kt
+++ app/src/main/java/com/example/labexam04/database/databsehelper.kt
@@ -5,15 +5,15 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class databsehelper(context: Context): SQLiteOpenHelper(context,DB_Name,null,DB_version) {
-  companion object{
-      private  val DB_Name="task"
-      private val DB_version= 1
-      private val TABLE_NAME="tasklist"
-      private val ID ="id"
-      private val TASK_NAME="taskname"
-      private  val TASK_DETAILS="taskdetails"
+    companion object{
+        private  val DB_Name="task"
+        private val DB_version= 1
+        private val TABLE_NAME="tasklist"
+        private val ID ="id"
+        private val TASK_NAME="taskname"
+        private  val TASK_DETAILS="taskdetails"
 
-  }
+    }
 
     override fun onCreate(db: SQLiteDatabase?) {
         val CREATE_TABLE = "createtable $TABLE_NAME($ID Integer,$TASK_NAME Text,$TASK_DETAILS Text);"
